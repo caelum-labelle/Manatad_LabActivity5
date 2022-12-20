ff5a9a6a4234bab7fdda17d083470769077fccba
Kyla Manatad <kylaguidomanatad@gmail.com>
2022-12-21 00:21:51 +0800

    Complete LeapYear

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -7,9 +7,17 @@ import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
 
     public static void main(String[] args) {
         FoodOrderGUI ch = new FoodOrderGUI();
@@ -18,5 +26,14 @@ public class FoodOrderGUI extends JFrame {
         ch.setDefaultCloseOperation(EXIT_ON_CLOSE);
         ch.setVisible(true);
         ch.setTitle("Food Ordering System");
+    }
+
+    public FoodOrderingSystem() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                orders();
+            }
+        });
     }
 }
