b370a846f502fb9af3091a775c9dd161f7abfba9
Kyla Manatad <kylaguidomanatad@gmail.com>
2022-12-20 23:38:05 +0800

    Rename FoodOrder components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,34 +1,9 @@
 package LeapYear;
 
 import javax.swing.*;
-import javax.swing.JTextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class LeapYearGUI extends JFrame {
+public class LeapYearGUI {
     private JPanel panel1;
-    private JTextField tfYear;
-    private JButton btnCheckYear;
-
-    public LeapYearGUI() {
-        btnCheckYear.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                checkYear();
-            }
-        });
-    }
-
-
-
-    public static void main(String[] args) {
-        LeapYearGUI ch = new LeapYearGUI();
-        ch.setContentPane(ch.panel1);
-        ch.setSize(350, 350);
-        ch.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ch.setVisible(true);
-        ch.setTitle("Leap Year Checker");
-    }
-
+    private JTextField textField1;
+    private JButton checkButton;
 }
-
--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,60 +1,10 @@
 package SimpleCalc;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class  SimpleCalcGUI extends JFrame {
+public class SimpleCalcGUI {
     private JPanel panel1;
-    private JTextField tfNumber1;
-    private JComboBox cbOperations;
-    private JButton btnCompute;
-    private JTextField tfNumber2;
-    private JTextField lblResult;
-
-    public SimpleCalcGUI() {
-        btnCompute.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                opConputer();
-            }
-        });
-    }
-
-    public void OpCompute() {
-        try {
-            String op = (String) cbOperations.getSelectedItem();
-            double a = Double.parseDouble(tfNumber1.getText());
-            double b = Double.parseDouble(tfNumber2.getText());
-            double res = 0;
-
-            if (op == "+") {
-                res = a + b;
-                lblResult.setText(String.valueOf(String.format("%.0f", (res))));
-            }
-            if (op == "-"){
-                res = a - b;
-                lblResult.setText(String.valueOf(String.format("%.0f", (res))));
-            }
-            if (op == "*"){
-                res = a * b;
-                lblResult.setText(String.valueOf(String.format("%.0f", (res))));
-            }
-            if (op == "/"){
-                res = a / b;
-                lblResult.setText(String.valueOf(String.format("%.0f", (res))));
-            }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
-        SimpleCalcGUI ch = new SimpleCalcGUI();
-        ch.setContentPane(ch.panel1);
-        ch.setSize(600, 300);
-        ch.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ch.setVisible(true);
-        ch.setTitle("Simple Calculator");
-    }
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton computeResultButton;
 }
